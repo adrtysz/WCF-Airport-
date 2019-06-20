2d859b82f150ddfc07c7b1f6586cb1450639fc97
Adżi <ada-tysz@wp.pl>
2019-06-21 00:31:33 +0200

    .

---
+++ Client/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ServiceModel;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AirportClient client = new AirportClient();
+
+            string fromCity;
+            string destinationCity;
+            string departureTime;
+            string arrivalTime;
+            string odp;
+            TimeSpan dT;
+            TimeSpan aT;
+
+
+            Console.WriteLine("WSZYSTKIE POLACZENIA - W, KONKRETNE POLACZENIE - K");
+            odp = Console.ReadLine();
+
+            if (odp.ToLower() == "k")
+            {
+
+                Console.WriteLine("Witaj, podaj miasta miedzy ktorymi chcesz znalezc polaczenie, oraz przedzial czasowy w formie HH:mm");
+                Console.WriteLine();
+
+                Console.WriteLine("Miasto startowe: ");
+                fromCity = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Miasto docelowe: ");
+                destinationCity = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Godzina odlotu: ");
+                departureTime = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Godzina przylotu na miejsce: ");
+                arrivalTime = Console.ReadLine();
+                Console.WriteLine();
+
+                dT = TimeSpan.Parse(departureTime);
+                aT = TimeSpan.Parse(arrivalTime);
+
+                try
+                {
+                    Timetable timetable = new Timetable();
+                    timetable.AddConnections(client.GetConnections(fromCity, destinationCity, dT, aT));
+                    timetable.Show();
+                }
+                catch (FaultException<ConnectionNotFoundFault> fault)
+                {
+                    Console.WriteLine(fault.Detail.Message);
+                }
+
+
+                Console.ReadKey();
+                client.Close();
+            } else if(odp.ToLower() == "w")
+            {
+                Console.WriteLine("Miasto startowe: ");
+                fromCity = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Miasto docelowe: ");
+                destinationCity = Console.ReadLine();
+                Console.WriteLine();
+
+            
+                dT = TimeSpan.Parse("00:00");
+                aT = TimeSpan.Parse("23:59");
+
+                try
+                {
+                    Timetable timetable = new Timetable();
+                    timetable.AddConnections(client.GetConnections(fromCity, destinationCity, dT, aT));
+                    timetable.Show();
+                }
+                catch (FaultException<ConnectionNotFoundFault> fault)
+                {
+                    Console.WriteLine(fault.Detail.Message);
+                }
+
+
+                Console.ReadKey();
+                client.Close();
+
+            }
+        } 
+    }
+}
---
+++ Client/Timetable.cs
@@ -0,0 +1,36 @@
+﻿using Client.ServiceReference1;
+using System;
+using System.Collections.Generic;
+
+
+namespace Client
+{
+    class Timetable
+    {
+        private List<Connection> _connections;
+
+        public Timetable()
+        {
+            _connections = new List<Connection>();
+        }
+
+        public void AddConnection(Connection connection)
+        {
+            _connections.Add(connection);
+        }
+
+        public void AddConnections(Connection[] connections)
+        {
+            _connections.AddRange(connections);
+        }
+
+        public void Show()
+        {
+            foreach (var c in _connections)
+            {
+                String s = String.Format("[{0} - {1}] to [{2} - {3}]", c.FromCity, c.StartTime, c.DestinationCity, c.ArrivalTime);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
